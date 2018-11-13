7e91537037641db1b34aec7d21960aceb55339f7
Gorgi Kosev <gorgi.kosev@gmail.com>
2018-11-13 17:02:41 +0000

    fix global to work in node too

--- global.js
+++ global.js
@@ -7,6 +7,9 @@ g = (function() {
 
 if (typeof window === "object") g = window;
 
+// node support
+else if (typeof global === 'object') g = global;
+
 // g can still be undefined, but nothing to do about it...
 // We return undefined, instead of nothing here, so it's
 // easier to handle this case. if(!global) { ...}
